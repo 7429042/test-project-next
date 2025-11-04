0557b9830d94d18f52b30bf877ceeafbcca21020
artem_goryanskiy <artemgoryanskiy@yandex.ru>
2025-11-04 19:46:49 +0300

    Add GitHub Actions workflow for deployment to GitHub Pages and static export configuration
    - Updated `next.config.ts` to enable static export with `output: 'export'` and adjust `basePath` and `assetPrefix` for production builds.
    - Created `.github/workflows/deploy.yml` for automating static site deployment to GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+const repo = 'test-project-next' // имя репозитория
+
+
 const nextConfig: NextConfig = {
+    output: 'export',              // обязательный статический экспорт
+    images: { unoptimized: true }, // отключаем серверную оптимизацию изображений
+    // Так как это "project site" (адрес будет /test-project-next/...),
+    // в проде нужны корректные базовые пути:
+    basePath: isProd ? `/${repo}` : undefined,
+    assetPrefix: isProd ? `/${repo}/` : undefined,
+    // при желании можно включить слэш на конце урлов:
+    // trailingSlash: true,
   /* config options here */
 };
 
